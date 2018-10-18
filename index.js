20bf50b24a57ae4fb4007f3c8a3251eb3e3e626d
GAbriel <jds>
2018-10-18 15:16:54 +0200

    adding users on server

--- index.js
+++ index.js
@@ -2,18 +2,30 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {}
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.broadcast.emit('hi');
+  socket.on("registration", function(name){
+     if (users.name){
+      console.log(name + ' is registered');
+      var pair = {};
+      pair.connection = socket;
+      users.name = pair;
+     }else{
+       console.log('user name: '+ name +' already exists');
+     }
+  });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
   });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    delete users[name];
+    console.log(name +' disconnected and deleted');
   });
 });
 
