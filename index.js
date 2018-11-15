918605a1e72db7242de2ac56ea2e898411069ac3
GAbriel <jds>
2018-11-15 22:46:45 +0100

    adding JSDoc to server

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-/* 
-Contributors: Gabriel Dimitrov, Julian Leuze
+/**
+* @name Server
+* @author: Julian Leuze
+* @author: Gabriel Dimitrov
 */
 
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const path = require('path');
 const ss = require('socket.io-stream');
-// var fs = require('fs');
 var request = require('request')
 
-// add folders to virtual namespace
+/**
+ * add folders to virtual namespace
+ */
 app.use(express.static('icons'));
 app.use(express.static('res'));
-// app.use(express.static('media'))
 
-// handle port on ibmcloud and stay compatible for local development
+/**
+ * handle port on ibmcloud and stay compatible for local development
+ */
 let port = process.env.PORT || 3000;
-// key-value user database to keep track of active users
+/**
+ * key-value user database to keep track of active users
+ */
 var users = {}
 
+/**
+ * this is a beauty function for adding a zero to the minutes of the server time
+ * @param {Object} dateObject
+ */
 function addLeadingZeroToMinutes(dateObject){
 	mins = dateObject.getMinutes();
     if(mins < 10){
@@ -29,29 +38,37 @@ function addLeadingZeroToMinutes(dateObject){
 	return mins;
 }
 
-function formatMessageData(data,userOnline){
-	var messageData = {};
-	messageData.userName = userOnline;
-	messageData.payload = data.payload;
-	var dateObj = new Date();
-	var time = dateObj.getHours() + ":" + addLeadingZeroToMinutes(dateObj);
-	console.log("time: " + time );
-	var date = dateObj.getUTCFullYear() + "-" + (dateObj.getUTCMonth() +1) + "-" + dateObj.getDate();
-	messageData.time = time;
-	messageData.date = date;
-	return messageData;
-}
-
+/**
+ * 
+ * formats incoming messages, adding a timestamp and a date 
+ * to the message and packing the message in JSON-Object
+ * @param {JSON} data
+ * @param {String} userOnline
+ * @param {Function} callback
+ * @param {String} messageID
+ */
 function sendMessageWithTimestamp (data,userOnline,callback,messageID){
+  var messageData = {};
+  messageData.userName = userOnline;
+  messageData.payload = data.payload;
   var dateObj = new Date();
   var time = dateObj.getHours() + ":" + dateObj.getMinutes();
+  console.log("time: " + time );
   var date = dateObj.getUTCFullYear() + "-" + (dateObj.getUTCMonth() +1) + "-" + dateObj.getDate();
-  var messageData = formatMessageData(data,userOnline);
+  messageData.time = time;
+  messageData.date = date;
   // callback({"time": time, "date": date},messageID) TODO: figure out why not
-	// working or remove
+  // working or remove
   return messageData;
 }
 
+/**
+ * 
+ * sends POST request to the tone analyzer service and 
+ * receives a response if the data evaluates to happiness or sadness
+ * this function contains are Promise(), in order that it can be called synchronously with await. 
+ * @param {JSON} data
+ */
 function analyzeMood(data) {
 	return new Promise(function (resolve, reject) {
 	console.log('analyzing mood...')
@@ -59,7 +76,7 @@ function analyzeMood(data) {
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		uri: 'https://zealous-bhabha.eu-de.mybluemix.net/tone',
+		uri: 'https://howareu.eu-de.mybluemix.net/tone',
 		body: `{ "texts": ["${data.payload}"]}`,
 		method: 'POST'
 	},
@@ -76,7 +93,15 @@ function analyzeMood(data) {
 console.log('analyzed mood...')
 }
 
-// SOCKET.IO METHODS
+// --------------------SOCKET.IO CALLBACK FUNCTIONS--------------------------
+
+/**
+ * Handles the disconnection of a user
+ * Deletes the user from the internal user list
+ * @param {Boolean} hasRegistrated 
+ * @param {String} userOnline 
+ * @param {Object} socket 
+ */
 function handleDisconnect(hasRegistrated, userOnline, socket) {
 	if (hasRegistrated) {
 		socket.broadcast.emit("userisgone",userOnline);
@@ -85,11 +110,15 @@ function handleDisconnect(hasRegistrated, userOnline, socket) {
 	}
 	console.log('user disconnected');
 }
-
-/*
+ 
+/**
+ * Delivers broadcast message to all users who are registered with socket.io
  * data-structure of param data: TODO: data = { }
- * 
- * 
+ * @param {JSON} data 
+ * @param {Function} callback 
+ * @param {String} messageID 
+ * @param {String} userOnline 
+ * @param {Object} socket 
  */
 function handleBroadcast(data, callback,messageID, userOnline, socket){
 	console.log(callback);
@@ -99,9 +128,13 @@ function handleBroadcast(data, callback,messageID, userOnline, socket){
     socket.broadcast.emit(data.emitName, messageData);
 }
 
-/*
+/**
+ * Delivers private message to a specific user, adding the mood of the sender 
  * Callback for when whisper is issued data-structure of param data see:
- * handleBroadcast
+ * @param {JSON} data 
+ * @param {Function} callback 
+ * @param {String} messageID 
+ * @param {String} userOnline 
  */
 async function handlePrivateMessage(data,callback,messageID, userOnline) {
 	console.log('callback: ' + callback)
@@ -113,6 +146,13 @@ async function handlePrivateMessage(data,callback,messageID, userOnline) {
 	io.to(data.id).emit('clientPrivateMessage', messageData);
 }
 
+/**
+ * Sends media files using streams in order not to save files on the server
+ * the server works only as a relay, their is no fishy business going on here, sadly NSA is always watching
+ * @param {Object} incomingStream 
+ * @param {JSON} data 
+ * @param {Object} socket 
+ */
 function handleSendingBinary(incomingStream, data,socket) {
 	console.log(data);
 	for(var i in io.sockets.connected) {
@@ -128,34 +168,50 @@ function handleSendingBinary(incomingStream, data,socket) {
 	    }
 }
 
-/*
+/**
  * This function will trigger a marked message in the private room of the
  * recipient whenever a file has been uploaded TODO: param
+ * @param {JSON} data
+ * @param {String} filename
  */
 function handlePrivateUpload(data, filename) {
 	console.log('server uploadnotice')
 	io.to(data.id).emit('clientPrivateUpload', {"userName": userOnline, 'filename': filename})
 }
 
-/*
+/**
  * This function will trigger a marked message in the global room whenever a
  * file has been uploaded TODO: param
+ * @param {String} filename
+ * @param {Object} socket
+ * @param {String} userOnline
  */
 function handleUpload(filename, socket, userOnline) {
 	console.log('server uploadnotice')
 	socket.broadcast.emit('clientUpload', {"userName": userOnline, 'filename': filename})
 }
 
-/*
+/**
  * Sends the received message to all connected sockets. On the client this
  * message will be displayed in the 'Global' tab. param msg the message from the
  * client socket to be broadcast
+ * @param {String} msg
  */
 function handleChatMessage(msg) {
 	console.log('message: ' + msg);
 	io.emit('chat message', userOnline + ": " + msg);
 }
 
+/**
+ *  handling the registration of a user
+ *  the user will be warned if the following rules are broken:
+ *  1. the user name has to be longer then 0 characters
+ *  2. the user name has to be unique
+ *  3. TODO: user name must match the pattern (...)
+ *  @param {String} name
+ *  @param {JSON} userInfo
+ *  @param {Object} socket
+ */
 function handleRegistration(name, userInfo, socket) {
 	var answer = {};
 	console.log(name + " tried to register")
@@ -197,14 +253,14 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-/* On User connects */
+/* handling socket.io processes, which includes event management, callbacks and message transfer */
 io.on('connection', function(socket) {
 	console.log('a user connected');
 	var userInfo = {
 			userOnline: null,
 			hasRegistrated: null
 	}
-	//See SOCKET.IO METHODS Comment
+	//See SOCKET.IO CALLBACK FUNCTIONS Comment
 	socket.on("registration", (name) => handleRegistration(name, userInfo, socket));
 	socket.on('chat message', (msg) => handleChatMessage(msg));
 	socket.on('disconnect', () => handleDisconnect(userInfo.hasRegistrated, userInfo.userOnline, socket));
@@ -216,7 +272,7 @@ io.on('connection', function(socket) {
 	
 });
 
-// Starting the server
+// Starting the server on specific port
 http.listen(port, function() {
 	console.log('listening on *:' + port);
 });
