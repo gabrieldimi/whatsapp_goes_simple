52fee2aec4422d701c5db69298bc28bcaca2e68f
GAbriel <jds>
2018-11-22 22:20:20 +0100

    adding db

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 * @author: Gabriel Dimitrov
 */
 
+var fs = require("fs");
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const ss = require('socket.io-stream');
 var request = require('request')
+/*require the ibm_db module*/
+var ibmdb = require('ibm_db');
 
 /**
  * add folders to virtual namespace
@@ -25,6 +28,7 @@ let port = process.env.PORT || 3000;
  * key-value user database to keep track of active users
  */
 var users = {}
+
 
 /**
  * this is a beauty function for adding a zero to the minutes of the server time
@@ -272,6 +276,56 @@ io.on('connection', function(socket) {
 	ss(socket).on('sendingbinary', (incomingStream, data) => handleSendingBinary(incomingStream, data,socket));
 	
 });
+
+function connectToDB(){
+	console.log("Test program to access DB2 sample database");
+
+	/*Connect to the database server
+	  param 1: The DSN string which has the details of database name to connect to, user id, password, hostname, portnumber 
+	  param 2: The Callback function to execute when connection attempt to the specified database is completed
+	*/
+	var credentialsUnparsed = fs.readFileSync("DBcredentials.json");
+	var credentialsParsed = JSON.parse(credentialsUnparsed);
+	ibmdb.open("DRIVER={DB2};DATABASE="+credentialsParsed.db+";UID="+credentialsParsed.username+";PWD="+credentialsParsed.password+";HOSTNAME="+credentialsParsed.hostname+";port="+credentialsParsed.port, function(err, conn)
+	{
+			if(err) {
+			/*
+			  On error in connection, log the error message on console 
+			*/
+				  console.error("error: ", err.message);
+			} else {
+	
+			/*
+				On successful connection issue the SQL query by calling the query() function on Database
+				param 1: The SQL query to be issued
+				param 2: The callback function to execute when the database server responds
+			*/
+			conn.query("select * from employee fetch first 5 rows only", function(err, employees, moreResultSets) {
+							console.log("FirstName \t\t LastName");
+				console.log("----------\t\t---------");
+	
+				/*
+					Loop through the employees list returned from the select query and print the First name and last name of the employee	
+				*/
+							for (var i=0;i<employees.length;i++)
+				{
+					console.log(employees[i].FIRSTNME, "\t\t", employees[i].LASTNAME);
+				}
+				console.log("-----------------------");
+	
+				/*
+					Close the connection to the database
+					param 1: The callback function to execute on completion of close function.
+				*/
+				conn.close(function(){
+					console.log("Connection Closed");
+				});
+			});
+		}
+	});
+	
+}
+
 
 // Starting the server on specific port
 http.listen(port, function() {
