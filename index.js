304082421960d370af07fd5038c6afc1130407ea
Julian Leuze <leuze.julian@gmail.com>
2018-11-23 03:55:35 +0100

    weblogger added

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const ss = require('socket.io-stream');
 var request = require('request')
+const logger = require('./log.js')
+
+logger.debugLevel = 'info';
+logger.log('info', 'logger test');
 
 /*require the ibm_db module*/
 var ibmdb = require('ibm_db');
@@ -27,7 +31,7 @@ app.use (function (req, res, next) {
   if (req.secure || process.env.BLUEMIX_REGION === undefined) {
     next();
   } else {
-    console.log('redirecting to https');
+    logger.log('info', 'redirecting to https');
     res.redirect('https://' + req.headers.host + req.url);
   }
 });
@@ -70,7 +74,7 @@ function sendMessageWithTimestamp (data,userOnline,callback,messageID){
   messageData.payload = data.payload;
   var dateObj = new Date();
   var time = dateObj.getHours() + ":" + dateObj.getMinutes();
-  console.log("time: " + time );
+  logger.log('info', "time: " + time );
   var date = dateObj.getUTCFullYear() + "-" + (dateObj.getUTCMonth() +1) + "-" + dateObj.getDate();
   messageData.time = time;
   messageData.date = date;
@@ -88,7 +92,7 @@ function sendMessageWithTimestamp (data,userOnline,callback,messageID){
  */
 function analyzeMood(data) {
 	return new Promise(function (resolve, reject) {
-	console.log('analyzing mood...')
+	logger.log('info', 'analyzing mood...')
 	request({
 		headers: {
 			'Content-Type': 'application/json'
@@ -99,7 +103,7 @@ function analyzeMood(data) {
 	},
     function (error, response, body) {
         if (!error && response.statusCode == 200) {
-			console.log("MOOD: " + body);
+			logger.log('info', "MOOD: " + body);
             resolve(body);
         } else {
         reject(error);
@@ -107,7 +111,7 @@ function analyzeMood(data) {
     }
 );
 	});
-console.log('analyzed mood...')
+logger.log('info', 'analyzed mood...')
 }
 
 // --------------------SOCKET.IO CALLBACK FUNCTIONS--------------------------
@@ -123,9 +127,9 @@ function handleDisconnect(hasRegistrated, userOnline, socket) {
 	if (hasRegistrated) {
 		socket.broadcast.emit("userisgone",userOnline);
 		delete users[userOnline];
-		console.log(userOnline + 'has been deleted');
+		logger.log('info', userOnline + 'has been deleted');
 	}
-	console.log('user disconnected');
+	logger.log('info', 'user disconnected');
 }
  
 /**
@@ -138,9 +142,9 @@ function handleDisconnect(hasRegistrated, userOnline, socket) {
  * @param {Object} socket 
  */
 function handleBroadcast(data, callback,messageID, userOnline, socket){
-	console.log(callback);
-	console.log(messageID)
-	console.log("broadcast: " + userOnline + ": " + data.payload);
+	logger.log('info', callback);
+	logger.log('info', messageID)
+	logger.log('info', "broadcast: " + userOnline + ": " + data.payload);
 	var messageData = sendMessageWithTimestamp (data,userOnline,callback,messageID);
     socket.broadcast.emit(data.emitName, messageData);
 }
@@ -154,11 +158,11 @@ function handleBroadcast(data, callback,messageID, userOnline, socket){
  * @param {String} userOnline 
  */
 async function handlePrivateMessage(data,callback,messageID, userOnline) {
-	console.log('callback: ' + callback)
+	logger.log('info', 'callback: ' + callback)
 	messageData = sendMessageWithTimestamp (data,userOnline,callback,messageID)
-	console.log("private message to " + data.id);
+	logger.log('info', "private message to " + data.id);
 	var toneAnalyzer =  await analyzeMood(data);
-	console.log("TYPE" + (typeof toneAnalyzer));
+	logger.log('info', "TYPE" + (typeof toneAnalyzer));
 	messageData.mood = JSON.parse(toneAnalyzer).mood;
 	io.to(data.id).emit('clientPrivateMessage', messageData);
 }
@@ -171,13 +175,13 @@ async function handlePrivateMessage(data,callback,messageID, userOnline) {
  * @param {Object} socket 
  */
 function handleSendingBinary(incomingStream, data,socket) {
-	console.log(data);
+	logger.log('info', data);
 	for(var i in io.sockets.connected) {
 	      // don't send the stream back to the initiator
 	      if (io.sockets.connected[i].id != socket.id)
 	      {
 	        var socketTo = io.sockets.connected[i];
-	        console.log('server pushing to ' + socketTo.id);
+	        logger.log('info', 'server pushing to ' + socketTo.id);
 	        var outgoingStream = ss.createStream();
 	        ss(socketTo).emit('serverPushMediaFile', outgoingStream, data);
 	        incomingStream.pipe(outgoingStream);
@@ -192,7 +196,7 @@ function handleSendingBinary(incomingStream, data,socket) {
  * @param {String} filename
  */
 function handlePrivateUpload(data, filename) {
-	console.log('server uploadnotice')
+	logger.log('info', 'server uploadnotice')
 	io.to(data.id).emit('clientPrivateUpload', {"userName": userOnline, 'filename': filename})
 }
 
@@ -204,7 +208,7 @@ function handlePrivateUpload(data, filename) {
  * @param {String} userOnline
  */
 function handleUpload(filename, socket, userOnline) {
-	console.log('server uploadnotice')
+	logger.log('info', 'server uploadnotice')
 	socket.broadcast.emit('clientUpload', {"userName": userOnline, 'filename': filename})
 }
 
@@ -215,7 +219,7 @@ function handleUpload(filename, socket, userOnline) {
  * @param {String} msg
  */
 function handleChatMessage(msg) {
-	console.log('message: ' + msg);
+	logger.log('info', 'message: ' + msg);
 	io.emit('chat message', userOnline + ": " + msg);
 }
 
@@ -232,8 +236,8 @@ var re = /^\w[ \w]*(?<=\w)$/
  */
 function handleRegistration(name, userInfo, socket) {
 	var answer = {};
-	console.log(name + " tried to register")
-	console.log(users)
+	logger.log('info', name + " tried to register")
+	logger.log('info', users)
 	if(re.test(name)) {
 
 		/*
@@ -242,15 +246,15 @@ function handleRegistration(name, userInfo, socket) {
 		var queryResult = {};
 		doesUserExist(name,function(err,result){
 			if(err){
-                console.log("does user exist error",err);
+                logger.log('info', "does user exist error",err);
 			}else{
 				queryResult.name = result;
-				console.log("does user exist",result);
+				logger.log('info', "does user exist",result);
 			}
 		});
-		console.log(queryResult.name);
+		logger.log('info', queryResult.name);
 		if (!queryResult.name) {
-			console.log(name + ' is registered');
+			logger.log('info', name + ' is registered');
 			var pair = {};
 			users[name] = pair;
 			pair.connection = socket.id;
@@ -264,19 +268,19 @@ function handleRegistration(name, userInfo, socket) {
 			newUser.name = name;
 			addUserToDB(name,function(err,result){
 				if(err){
-					console.log("adding user error",err);
+					logger.log('info', "adding user error",err);
 				}else{
-					console.log("user added",result);
+					logger.log('info', "user added",result);
 				}
 			});
 			socket.broadcast.emit('newuser',newUser);
 		} else {
-			console.log('user name: ' + name + ' already exists');
+			logger.log('info', 'user name: ' + name + ' already exists');
 			answer.success = false;
 			answer.msg = name + " already exists";
 		}
     } else {
-		console.log('user name: '+ name + ' doesn\'t match pattern');
+		logger.log('info', 'user name: '+ name + ' doesn\'t match pattern');
 		answer.success = false;
 		answer.msg = name + " doesnt't match the pattern: at least one character, and may not end or start with whitspace";
 	}
@@ -284,13 +288,25 @@ function handleRegistration(name, userInfo, socket) {
 }
 
 app.get('/', function(req, res) {
-	console.log("Client IP: " + req.connection.remoteAddress)
+	logger.log('info', "Client IP: " + req.connection.remoteAddress)
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/weblogger', function(req,res) {
+	logger.log('info', "weblogger accessed");
+	res.sendFile(__dirname + '/weblogger.html')
+});
+
+var socketWeblogger = io.of('/weblogger');
+socketWeblogger.on('connection',function(socket) {
+	logger.log('info', 'weblogger listening')
+});
+logger.socket = socketWeblogger;
+
+
 /* handling socket.io processes, which includes event management, callbacks and message transfer */
 io.on('connection', function(socket) {
-	console.log('a user connected');
+	logger.log('info', 'a user connected');
 	var userInfo = {
 			userOnline: null,
 			hasRegistrated: null
@@ -308,7 +324,7 @@ io.on('connection', function(socket) {
 });
 
 function connectToDB(){
-	console.log("Test program to access DB2 sample database");
+	logger.log('info', "Test program to access DB2 sample database");
 
 	/*Connect to the database server
 	  param 1: The DSN string which has the details of database name to connect to, user id, password, hostname, portnumber 
@@ -324,7 +340,7 @@ function connectToDB(){
 			*/
 				  console.error("error: ", err.message);
 			} else {
-				console.log("testing bro");
+				logger.log('info', "testing bro");
 	
 			/*
 				On successful connection issue 
@@ -334,7 +350,7 @@ function connectToDB(){
 			databaseConnection = conn;
 			
 				// conn.close(function(){
-				// 	console.log("Connection Closed");
+				// 	logger.log('info', "Connection Closed");
 				// });
 		}
 	});
@@ -354,5 +370,5 @@ connectToDB();
 
 // Starting the server on specific port
 http.listen(port, function() {
-	console.log('listening on *:' + port);
+	logger.log('info', 'listening on *:' + port);
 });
---
+++ log.js
@@ -0,0 +1,15 @@
+  var logger = exports;
+  logger.debugLevel = 'warn';
+  logger.socket;
+  logger.log = function(level, message) {
+    var levels = ['error', 'warn', 'info'];
+    if (levels.indexOf(level) >= levels.indexOf(logger.debugLevel) ) {
+      if (typeof message !== 'string') {
+        message = JSON.stringify(message);
+      };
+      console.log(level+': '+message);
+      if(logger.socket) {
+        logger.socket.emit('log', level+': '+message);
+      }
+    }
+  }
